66048236c29bebab91a20f0ec9862bdc8af0de6a
Abdalla <abdallahsharaf95@gmail.com>
2025-02-13 21:12:14 +0200

    Update page.tsx

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import { Product, ProductsTable } from '@/components/dashboard/products/products-table';
+import { Product } from '@/components/dashboard/products/products-table';
 import * as React from 'react';
 import type { Metadata } from 'next';
 import Stack from '@mui/material/Stack';
