199261e579ccd3e4985b994b919bde7065997c97
Abdalla <abdallahsharaf95@gmail.com>
2025-02-15 19:59:59 +0200

    commit

--- src/app/dashboard/products/metadata.ts
+++
@@ -1,6 +0,0 @@
-import type { Metadata } from 'next';
-import { config } from '@/config';
-
-export const metadata: Metadata = {
-  title: `Products | Dashboard | ${config.site.name}`,
-};
--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,10 +1,14 @@
-'use client'
-
 import * as React from 'react';
 import { ProductsTable } from '@/components/dashboard/products/products-table';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
+import type { Metadata } from 'next';
+import { config } from '@/config';
+
+export const metadata: Metadata = {
+  title: `Products | Dashboard | ${config.site.name}`,
+};
 
 export default function Page(): React.JSX.Element {
   return (
