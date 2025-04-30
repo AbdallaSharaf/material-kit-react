0bbecbe3a2d6e2bc33424a2ec123937b127df91d
Abdalla <abdallahsharaf95@gmail.com>
2025-04-30 20:34:02 +0300

    Update page.tsx

--- src/app/dashboard/customers/page.tsx
+++ src/app/dashboard/customers/page.tsx
@@ -6,103 +6,10 @@ import Typography from '@mui/material/Typography';
 
 import { config } from '@/config';
 import { CustomersTable } from '@/components/dashboard/customer/customers-table';
-import type { Customer } from '@/components/dashboard/customer/customers-table';
 import { useTranslations } from 'next-intl';
 
 export const metadata = { title: `Customers | Dashboard | ${config.site.name}` } satisfies Metadata;
 
-const customers = [
-  {
-    id: 'USR-010',
-    name: 'Alcides Antonio',
-    avatar: '/assets/avatar-10.png',
-    email: 'alcides.antonio@devias.io',
-    phone: '908-691-3242',
-    ordersCount: 5,
-    totalSpent: 1200,
-  },
-  {
-    id: 'USR-009',
-    name: 'Marcus Finn',
-    avatar: '/assets/avatar-9.png',
-    email: 'marcus.finn@devias.io',
-    phone: '415-907-2647',
-    ordersCount: 3,
-    totalSpent: 850,
-  },
-  {
-    id: 'USR-008',
-    name: 'Jie Yan',
-    avatar: '/assets/avatar-8.png',
-    email: 'jie.yan.song@devias.io',
-    phone: '770-635-2682',
-    ordersCount: 10,
-    totalSpent: 2200,
-  },
-  {
-    id: 'USR-007',
-    name: 'Nasimiyu Danai',
-    avatar: '/assets/avatar-7.png',
-    email: 'nasimiyu.danai@devias.io',
-    phone: '801-301-7894',
-    ordersCount: 7,
-    totalSpent: 1500,
-  },
-  {
-    id: 'USR-006',
-    name: 'Iulia Albu',
-    avatar: '/assets/avatar-6.png',
-    email: 'iulia.albu@devias.io',
-    phone: '313-812-8947',
-    ordersCount: 2,
-    totalSpent: 450,
-  },
-  {
-    id: 'USR-005',
-    name: 'Fran Perez',
-    avatar: '/assets/avatar-5.png',
-    email: 'fran.perez@devias.io',
-    phone: '712-351-5711',
-    ordersCount: 4,
-    totalSpent: 980,
-  },
-  {
-    id: 'USR-004',
-    name: 'Penjani Inyene',
-    avatar: '/assets/avatar-4.png',
-    email: 'penjani.inyene@devias.io',
-    phone: '858-602-3409',
-    ordersCount: 6,
-    totalSpent: 1450,
-  },
-  {
-    id: 'USR-003',
-    name: 'Carson Darrin',
-    avatar: '/assets/avatar-3.png',
-    email: 'carson.darrin@devias.io',
-    phone: '304-428-3097',
-    ordersCount: 8,
-    totalSpent: 1750,
-  },
-  {
-    id: 'USR-002',
-    name: 'Siegbert Gottfried',
-    avatar: '/assets/avatar-2.png',
-    email: 'siegbert.gottfried@devias.io',
-    phone: '702-661-1654',
-    ordersCount: 12,
-    totalSpent: 3000,
-  },
-  {
-    id: 'USR-001',
-    name: 'Miron Vitold',
-    avatar: '/assets/avatar-1.png',
-    email: 'miron.vitold@devias.io',
-    phone: '972-333-4106',
-    ordersCount: 9,
-    totalSpent: 2000,
-  },
-] satisfies Customer[];
 
 export default function Page(): React.JSX.Element {
   const t = useTranslations('common');
@@ -114,7 +21,6 @@ export default function Page(): React.JSX.Element {
         </Stack>
       </Stack>
       <CustomersTable
-        data={customers}
       />
     </Stack>
   );
